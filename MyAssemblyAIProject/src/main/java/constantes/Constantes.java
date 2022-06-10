b9f41d2c7b4060d6c0e8f57d81dc3c924455ad47
Jim <Jim@DESKTOP-JI7DO2S.home>
2022-06-10 20:17:49 +0200

    Ajout Constantes, attributs du transcrit + fin de l'algo

--- MyAssemblyAIProject/src/main/java/constantes/Constantes.java
+++ MyAssemblyAIProject/src/main/java/constantes/Constantes.java
@@ -2,7 +2,22 @@ package constantes;
 
 public class Constantes
 {
+	public static String POST_URI = "https://api.assemblyai.com/v2/transcript";
+
+	public static String GET_URI = "https://api.assemblyai.com/v2/transcript/";
+
+	public static String HEADER_AUTHORIZATION = "Authorization";
+
 	public static String CLE_API = "dcd71d26bf514f758698af543f82910a";
 
-	public static String URL_AUDIO = "";
+	public static String URL_AUDIO = "https://github.com/Merray/MyAssemblyAI/raw/main/MyAssemblyAIProject/src/main/resources/audios/testAudio.mp3";
+
+	/*
+	 * https://docs.assemblyai.com/#supported-languages
+	 */
+	public static String CODE_LANGUE = "fr";
+
+	// En millisecondes : 1sec
+	public static int TEMPS_AVANT_NOUVELLE_REQUETE = 1000;
+
 }
--- MyAssemblyAIProject/src/main/java/mainApp/MainApp.java
+++ MyAssemblyAIProject/src/main/java/mainApp/MainApp.java
@@ -1,19 +1,69 @@
 package mainApp;
 
 import java.net.URI;
-import java.net.URISyntaxException;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import com.google.gson.Gson;
 
 import constantes.Constantes;
+import entites.Transcrit;
 
 public class MainApp
 {
 
-	public static void main(String[] args) throws URISyntaxException
+	public static void main(String[] args) throws Exception
 	{
-		HttpRequest requetePost = HttpRequest.newBuilder().uri(new URI("https://api.assemblyai.com/v2/transcript"))
-				.header("Autorization", Constantes.CLE_API).POST(BodyPublishers.ofString(null)).build();
+
+		Transcrit transcrit = new Transcrit();
+		transcrit.setAudio_url(Constantes.URL_AUDIO);
+		transcrit.setLanguage_code(Constantes.CODE_LANGUE);
+
+		Gson gson = new Gson();
+		String leJson = gson.toJson(transcrit);
+
+		System.out.println("Appel POST : " + Constantes.POST_URI);
+		System.out.println("Body envoyé dans le post : " + leJson);
+
+		HttpRequest requetePost = HttpRequest.newBuilder().uri(new URI(Constantes.POST_URI))
+				.header(Constantes.HEADER_AUTHORIZATION, Constantes.CLE_API).POST(BodyPublishers.ofString(leJson))
+				.build();
+
+		HttpClient httpClient = HttpClient.newHttpClient();
+
+		HttpResponse<String> reponsePost = httpClient.send(requetePost, BodyHandlers.ofString());
+
+		System.out.println("POST : Body récupéré : " + reponsePost.body());
+
+		transcrit = gson.fromJson(reponsePost.body(), Transcrit.class);
+
+		System.out.println("Appel GET : " + Constantes.GET_URI + transcrit.getId());
+
+		HttpRequest requeteGet = HttpRequest.newBuilder().uri(new URI(Constantes.GET_URI + transcrit.getId()))
+				.header("Authorization", Constantes.CLE_API).build();
+
+		while (true)
+		{
+			HttpResponse<String> reponseGet = httpClient.send(requeteGet, BodyHandlers.ofString());
+			transcrit = gson.fromJson(reponseGet.body(), Transcrit.class);
+
+			System.out.println("GET : Body récupéré : " + reponsePost.body());
+			System.out.println("Status : " + transcrit.getStatus());
+
+			if ("completed".equals(transcrit.getStatus()) || "error".equals(transcrit.getStatus()))
+			{
+				break;
+			}
+
+			Thread.sleep(Constantes.TEMPS_AVANT_NOUVELLE_REQUETE);
+		}
+
+		System.out.println("Fin de transcription !\n");
+		System.out.println("Resultats : " + transcrit.getText());
+
 	}
 
 }
